ada9e4056f21a9600015ef3daedca6c28dc1f50f
Gerardo Gonzalez <gerardocampeon1@hotmail.com>
2026-03-31 16:15:24 -0500

    add: primera pagina de landing page

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -1,10 +1,16 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@tailwindcss/vite';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  // Mantenemos react para que los iconos carguen sin problemas
+  integrations: [react()],
   vite: {
     plugins: [tailwind()],
+    // Forzamos a Vite a optimizar react-icons para evitar errores de transformación
+    ssr: {
+      noExternal: ['react-icons']
+    }
   },
 });
